685dfacaa2a0d5c33760cdac396ce43cd041db8b
LsSuarez <luis_suarez5@usmp.pe>
2025-06-07 21:20:36 -0500

    Inicializa proyecto SUAREZ ECOMMERCE PC4 con diseño y estructura base

---
+++ Data/Migrations/20250608021226_AddProductoUsuario.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace practicaml.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductoUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
